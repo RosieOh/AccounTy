a28e06c08b26fd7ac3887923d3c414dffb5fa4e9
RosieOh <20172207@gm.hannam.ac.kr>
2024-08-25 01:44:02 +0900

    Initial Commit

---
+++ src/main/java/com/accounty/AccounTyApplication.java
@@ -0,0 +1,13 @@
+package com.accounty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccounTyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccounTyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accounty/AccounTyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccounTyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
