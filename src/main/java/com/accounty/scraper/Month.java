66460d86225f8ecbf6b41e6d9367ab40d0c137e9
RosieOh <20172207@gm.hannam.ac.kr>
2024-08-25 02:06:29 +0900

    ✨FEAT : 월 단위 컴포지션 추가

---
+++ src/main/java/com/accounty/scraper/Month.java
@@ -0,0 +1,24 @@
+package com.accounty.scraper;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Month {
+    JAN("Jan", 1),
+    FEB("Feb", 2),
+    MAR("Mar", 3),
+    APR("Apr", 4),
+    MAY("May", 5),
+    JUN("Jun", 6),
+    JUL("Jul", 7),
+    AUG("Aug", 8),
+    SEP("Sep", 9),
+    OCT("Oct", 10),
+    NOV("Nov", 11),
+    DEC("Dec", 12);
+
+    private String month;
+    private int monthToNumber;
+}
