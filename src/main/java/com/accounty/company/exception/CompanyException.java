decf8686113c81994a5d0363a0dda1600affad85
RosieOh <20172207@gm.hannam.ac.kr>
2024-08-25 11:13:32 +0900

    ✨FEAT : 회사 DB 조회 실패에 따른 예외처리

---
+++ src/main/java/com/accounty/company/exception/CompanyException.java
@@ -0,0 +1,11 @@
+package com.accounty.company.exception;
+
+public class CompanyException extends RuntimeException {
+    public CompanyException() {
+        super();
+    }
+
+    public CompanyException(String message) {
+        super(message);
+    }
+}
