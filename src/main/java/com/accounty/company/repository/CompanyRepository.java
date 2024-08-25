65f4af165edb85338e346e4a0c12547dc5d8db1c
RosieOh <20172207@gm.hannam.ac.kr>
2024-08-25 23:40:07 +0900

    FEAT | #2 | 회사 자동 검색 비즈니스 레이어 추가 및 기존 메소드 분리

--- src/main/java/com/accounty/company/repository/CompanyRepository.java
+++ src/main/java/com/accounty/company/repository/CompanyRepository.java
@@ -4,12 +4,19 @@ import com.accounty.company.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-    Optional<Company> findByTicketAndDelete(String ticker, LocalDateTime delDate);
+    Optional<Company> findByTicker(String ticker);
 
-    Optional<Company> findByNameAndDelete(String name, LocalDateTime delDate);
+    List<Company> findAllByTicker(String name);
+
+    Optional<Company> findByName(String name);
+
+    List<Company> findTop10ByNameLikeOrderByNameDesc(String prefix);
+
+
+
 }
