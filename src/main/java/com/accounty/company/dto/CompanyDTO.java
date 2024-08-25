da151596a59a276d51723170d5b8c04c1b674f32
RosieOh <20172207@gm.hannam.ac.kr>
2024-08-25 11:12:20 +0900

    ✨FEAT : 회사명 요청 DTO 객체 생성

---
+++ src/main/java/com/accounty/company/dto/CompanyDTO.java
@@ -0,0 +1,26 @@
+package com.accounty.company.dto;
+
+import lombok.*;
+
+public class CompanyDTO {
+    @Getter
+    @Setter // 테스트용
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    @ToString
+    public static class Request {
+        private String ticker;
+    }
+
+    @Getter
+    @Setter // 테스트용
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    @ToString
+    public static class Response {
+        private String companyName;
+        private String ticker;
+    }
+}
