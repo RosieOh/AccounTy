427154fbb8d2aff043f4e918158bb453298bd5aa
RosieOh <20172207@gm.hannam.ac.kr>
2024-09-07 17:42:39 +0900

    CHORE | #4 | ApiResponse 성공시 매개변수가 아닌 리스트로 덤프 시키는 로직 수정

--- src/main/java/com/accounty/global/GlobalApiResponse.java
+++ src/main/java/com/accounty/global/GlobalApiResponse.java
@@ -10,13 +10,13 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder(toBuilder = true)
 @ToString
-public class GlobalApiResponse {
+public class GlobalApiResponse<T> {
     private int status;
     private String message;
-    private List<?> result;
+    private T result;
 
     // api 응답 성공 시 apiResponse로 반환 메소드
-    public static GlobalApiResponse toGlobalApiResponse(List<?> dtoList) {
+    public static <T> GlobalApiResponse toGlobalApiResponse(T dtoList) {
         return GlobalApiResponse.builder()
                 .message("성공")
                 .status(200)
