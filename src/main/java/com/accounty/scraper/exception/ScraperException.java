908f2e51f68e5572d8bd1fd92c96af47b1e6acfe
RosieOh <20172207@gm.hannam.ac.kr>
2024-09-07 17:44:55 +0900

    FEAT | #5| 크롤링 예외처리 로직 구현

---
+++ src/main/java/com/accounty/scraper/exception/ScraperException.java
@@ -0,0 +1,15 @@
+package com.accounty.scraper.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ScraperException extends RuntimeException{
+    private String message;
+    private ScraperErrorCode scraperErrorCode;
+
+    public ScraperException(ScraperErrorCode scraperErrorCode, String message) {
+        super(message);
+        this.message = message;
+        this.scraperErrorCode = scraperErrorCode;
+    }
+}
